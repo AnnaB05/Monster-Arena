54960daefa8f721a7a2e6d224fee560b80ac7869
AnnaB05 <aebake05@louisville.edu>
2025-10-13 21:56:45 -0400

    opulated Rarity file. There are now 6 rarity types: common to Legendary.

--- MonsterArena/src/main/java/io/github/AnnaB05/monsterArena/model/Rarity.java
+++ MonsterArena/src/main/java/io/github/AnnaB05/monsterArena/model/Rarity.java
@@ -1,4 +1,25 @@
 package io.github.AnnaB05.monsterArena.model;
 
+/**
+ * Enum representing the rarity levels of monsters, each with an associated color and pull chance.
+ */
 public enum Rarity {
+    COMMON("Gray", 0.50),
+    UNCOMMON("Green", 0.25),
+    RARE("Blue", 0.15),
+    SUPER_RARE("Purple", 0.07),
+    EPIC("Gold", 0.02),
+    LEGENDARY("Red", 0.01);
+
+    private final String color;
+    private final double pullChance;
+
+    Rarity(String color, double pullChance) {
+        this.color = color;
+        this.pullChance = pullChance;
+    }
+
+    public String getColor() { return color; }
+    public double getPullChance() { return pullChance; }
 }
+
